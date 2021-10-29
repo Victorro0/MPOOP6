dbe507bc4cf367a71570a2c9fc1b3f3e1a13f3b2
VictorRodrigu46 <>
2021-10-29 00:15:58 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+/**
+*@author Rodriguez Escobar Victor<
+*/
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    System.out.println("La suma es: "+suma(2,2));
+  }
+
+/**
+*@param a primer entero de la ecuacion 
+*@param b segundo entero de la suma 
+*@return La suma de los enteros a mas b
+*/
+  public static int suma(int a, int b){
+    return a+b;
+  }
+}
